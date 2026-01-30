8850f3922b635e95d5d0213b16d6688fcae5334e
hamzaboulaamane <hamzaboulaamane06@gmail.com>
2026-01-30 20:09:03 +0000

    Final CV website

--- script.js
+++ script.js
@@ -1,16 +1,2 @@
-// Auto update year
-const yearSpan = document.getElementById("year");
-yearSpan.textContent = new Date().getFullYear();
-
-// Dark mode toggle
-const toggleBtn = document.getElementById("darkModeBtn");
-
-toggleBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-
-  if (document.body.classList.contains("dark-mode")) {
-    toggleBtn.textContent = "Light mode ☀️";
-  } else {
-    toggleBtn.textContent = "Dark mode 🌙";
-  }
-});
+// Automatic Year in Footer
+document.getElementById('year').textContent = new Date().getFullYear();
